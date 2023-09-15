fcef5430940566cd81fe9ef2560dbcba1cc4fbaf
Jose Albornoz <prueba@outlook.cl>
2023-09-14 23:34:33 -0300

    Ver si me funciona bien el componente NotFound en ghpages

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,22 @@ import './App.css';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './componentes/Navbar';
-import Home from "./views/Home"
-import Contact from "./views/Contact"
+import Home from './views/Home';
+import Contact from './views/Contact';
 import NotFound from './views/NotFound';
 
 function App() {
-
-  // Url de base para que en Github al hacer el deploy en gh-pages no se cambie la ruta 
-  const URL_GHPAGES = '/desafio1-happycake/'
+  // Url de base para que en Github al hacer el deploy en gh-pages no se cambie la ruta
+  const URL_GHPAGES = '/desafio1-happycake/';
 
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path={URL_GHPAGES} element={<Home/>}/>
-          <Route path={`${URL_GHPAGES}contacto`} element={<Contact/>}/>
-          <Route path={`${URL_GHPAGES}*`} element={<NotFound />} />
+          <Route path={URL_GHPAGES} element={<Home />} />
+          <Route path={`${URL_GHPAGES}contacto`} element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
